6d5efc00c43d3c478793d40fbdc009023d677afa
Artyom <snayperros@gmail.com>
2022-03-30 21:21:00 +0300

    server and client v0.00002 теперь сервер умеет реагировать на команды

--- src/main/java/com/art/client/Client.java
+++ src/main/java/com/art/client/Client.java
@@ -2,7 +2,7 @@ package com.art.client;
 
 public class Client {
     public static void main(String[] args) throws InterruptedException {
-        NetWork client = new NetWork();
+        NetWork client = new NetWork();//
     }
 
 }
